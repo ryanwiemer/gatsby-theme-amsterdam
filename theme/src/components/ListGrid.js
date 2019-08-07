d88058f96be3b75f3be6ff62f3efaad531ad33dc
ryanwiemer <ryan@ryanwiemer.com>
2019-08-06 23:21:51 -0700

    Adjust ListGrid styling

--- theme/src/components/ListGrid.js
+++ theme/src/components/ListGrid.js
@@ -3,6 +3,7 @@ import { Link } from 'gatsby'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
 import Placeholder from './Placeholder'
+const _ = require(`lodash`)
 
 const List = styled.ul`
   margin: 3rem auto;
@@ -11,11 +12,11 @@ const List = styled.ul`
   display: flex;
   flex-flow: row wrap;
   justify-content: space-between;
-  li:first-child a {
+  li:first-child {
     border: 0;
     padding-top: 0;
   }
-  li:last-child a {
+  li:last-child {
     padding-bottom: 0;
     margin-bottom: 2rem;
   }
@@ -25,6 +26,12 @@ const Cover = styled(Img)`
   transition: opacity 0.4s;
   img {
     transition: transform 0.6s !important;
+  }
+  &:hover {
+    opacity: 0.8;
+    img {
+      transform: scale(1.03);
+    }
   }
   @media (hover: none) {
     opacity: 1 !important;
@@ -40,8 +47,13 @@ const Item = styled.li`
   flex: 0 100%;
   margin: 0 0 2rem;
   @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    flex: 0 100%;
     margin: 0;
+    width: 100%;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: space-between;
+    padding: 2rem 0;
+    border-top: 1px solid ${props => props.theme.colors.border};
   }
   a {
     text-decoration: none;
@@ -49,12 +61,6 @@ const Item = styled.li`
     color: ${props => props.theme.colors.base};
     &:hover {
       color: ${props => props.theme.colors.highlight};
-      ${Cover} {
-        opacity: 0.8;
-        img {
-          transform: scale(1.03);
-        }
-      }
     }
     @media (hover: none) {
       color: ${props => props.theme.colors.base} !important;
@@ -80,27 +86,42 @@ const Excerpt = styled.p`
   color: ${props => props.theme.colors.secondary};
 `
 
-const StyledLink = styled(Link)`
-  @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    width: 100%;
-    display: flex;
-    flex-flow: row wrap;
-    justify-content: space-between;
-    padding: 2rem 0;
-    border-top: 1px solid ${props => props.theme.colors.border};
-  }
-`
-
 const ImageContainer = styled.div`
   @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    flex: 0 50%;
+    flex: 0 34%;
   }
 `
 
 const TextContainer = styled.div`
   @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    padding: 0 0 0 1rem;
-    flex: 0 50%;
+    padding: 0 0 0 2rem;
+    flex: 0 66%;
+  }
+`
+
+const AdditionalContainer = styled.div`
+  padding: 1rem 0 0;
+  line-height: 1.5;
+  font-size: 0.9em;
+`
+
+const Divider = styled.span`
+  margin: 0 0.25rem;
+  color: ${props => props.theme.colors.border};
+`
+
+const Date = styled.span`
+  display: inline-block;
+  color: ${props => props.theme.colors.tertiary};
+`
+
+const Tag = styled.span`
+  display: inline-block;
+  margin: 0 0.5rem 0 0;
+  text-transform: capitalize;
+  a {
+    text-decoration: underline;
+    color: ${props => props.theme.colors.tertiary};
   }
 `
 
@@ -109,8 +130,8 @@ const ListGrid = props => {
     <List>
       {props.posts.map(({ node: post }) => (
         <Item key={post.frontmatter.title}>
-          <StyledLink to={post.fields.slug}>
-            <ImageContainer>
+          <ImageContainer>
+            <Link to={post.fields.slug}>
               {post.frontmatter.cover && (
                 <Cover
                   sizes={{
@@ -125,12 +146,27 @@ const ListGrid = props => {
               ) : (
                 ''
               )}
-            </ImageContainer>
-            <TextContainer>
+            </Link>
+          </ImageContainer>
+          <TextContainer>
+            <Link to={post.fields.slug}>
               <Title>{post.frontmatter.title}</Title>
               <Excerpt>{post.excerpt}</Excerpt>
-            </TextContainer>
-          </StyledLink>
+            </Link>
+            <AdditionalContainer>
+              {post.frontmatter.date && <Date>{post.frontmatter.date}</Date>}
+              {post.frontmatter.tags && (
+                <>
+                  <Divider>/</Divider>
+                  {post.frontmatter.tags.map(tag => (
+                    <Tag key={tag}>
+                      <Link to={`/tag/${_.kebabCase(tag)}/`}>{tag}</Link>
+                    </Tag>
+                  ))}
+                </>
+              )}
+            </AdditionalContainer>
+          </TextContainer>
         </Item>
       ))}
     </List>
